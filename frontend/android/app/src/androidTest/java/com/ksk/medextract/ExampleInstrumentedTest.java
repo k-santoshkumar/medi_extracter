37fcb7b6f7b7485528cc9a25fd7a84dc0912461f
k-santoshkumar <santoshkumarkuricheti@gmail.com>
2026-04-18 19:11:16 +0530

    refactor: rename Android package to com.ksk.medextract and update app display name

--- frontend/android/app/src/androidTest/java/com/ksk/medextract/ExampleInstrumentedTest.java
+++ frontend/android/app/src/androidTest/java/com/ksk/medextract/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.getcapacitor.myapp;
+package com.ksk.medextract;
 
 import static org.junit.Assert.*;
 
--- frontend/android/app/src/main/java/com/ksk/medextract/MainActivity.java
+++ frontend/android/app/src/main/java/com/ksk/medextract/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package com.ksk.medextract;
 
 import com.getcapacitor.BridgeActivity;
 
